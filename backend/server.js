0b2a013a70c6ac98b0116d29939d7af6f303f888
1405Abhi <abhijeetpatil4599@gmail.com>
2025-11-06 11:15:04 +0530

    changes_in_server

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use("/api/hotels", require("./routes/hotels"));
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) =>
+  app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
   );
 }
